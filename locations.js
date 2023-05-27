781a08d204105d77c4d01f677042ba629154b483
Bill Morris <bill@billmorris.io>
2023-05-27 14:43:17 -0400

    separate location config

---
+++ locations.js
@@ -0,0 +1,26 @@
+var locations = [
+    {
+      name: 'middlebury',
+      bearing: 238
+    },
+    {
+      name: 'vuelta larga',
+      bearing: 175 
+    },
+    {
+      name: 'boston',
+      bearing: 162
+    },
+    {
+      name: 'harwichport',
+      bearing: 152
+    },
+    {
+      name: 'mont albert',
+      bearing: 40
+    },
+    { 
+      name: 'central city',
+      bearing: 270
+    }
+  ]
